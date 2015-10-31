02e6c3da4bfd164fec9c03ba093340971c235b63
Boris Kaiser <mail@boriskaiser.com>
2015-10-31 12:06:26 +0100

    Add first tests with Karma

---
+++ tddjs/chapter2/karma/spec/test.js
@@ -0,0 +1,10 @@
+describe("test", function () {
+    it('Addition of 1 and 1', function () {
+        expect(add(1, 1)).toEqual(2);
+    });
+
+    it('Addition of 1 and 3', function () {
+        expect(add(1, 3)).toEqual(5);
+    });
+});
+
