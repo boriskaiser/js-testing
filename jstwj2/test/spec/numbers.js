96e0e7edaa46d13e9b1bf3ea10a9acaba9787d49
Boris Kaiser <mail@boriskaiser.com>
2015-11-01 12:04:52 +0100

    Add setup for jstwj2

---
+++ jstwj2/test/spec/numbers.js
@@ -0,0 +1,15 @@
+/*globals define, describe*/
+
+define(['numbers', function (numbers) {
+    'use strict';
+
+    describe('The numbers module', function () {
+
+        describe('The add method', function () {
+
+
+
+        });
+
+    });
+}]);
